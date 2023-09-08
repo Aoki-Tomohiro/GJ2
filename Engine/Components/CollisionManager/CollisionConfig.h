3a67488fd006c35868ed2b9c58d3d6a6b32cee41
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-09-08 19:22:01 +0900

    CollisionConfig訂正

--- Engine/Components/CollisionManager/CollisionConfig.h
+++ Engine/Components/CollisionManager/CollisionConfig.h
@@ -2,12 +2,12 @@
 #include <cstdint>
 
 //プレイヤー陣営
-const uint32_t kCollisionAttributePlayer = 0b1;
+const uint32_t kCollisionAttributePlayer       = 0b0001;
 //プレイヤー球
-const uint32_t kCollisionAttributePlayerBullet = 0b11;
+const uint32_t kCollisionAttributePlayerBullet = 0b1011;
 
 
 //敵本隊
-const uint32_t kCollisionAttributeEnemy = 0b100;
+const uint32_t kCollisionAttributeEnemy        = 0b0100;
 //敵の球
-const uint32_t kCollisionAttributeEnemyBullet = 0b101;
+const uint32_t kCollisionAttributeEnemyBullet  = 0b1111;

f43a5f0ad9212e53845f71617b88710b305c997f
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-09-09 18:03:44 +0900

    動き完成

--- Project/GameObject/Enemy/TransCube/state/TransCubeGroundAttak/TransCubeGroundAttackState.cpp
+++ Project/GameObject/Enemy/TransCube/state/TransCubeGroundAttak/TransCubeGroundAttackState.cpp
@@ -1,45 +1,31 @@
 #include"TranscubeGroundAttackState.h"
 
-
-
-
 void TransCubeGroundAttackState::Initialize(TransCube* state)
 {
 	 CoolTime = 0;
 	 BulletCount = 0;
-
 }
 
-void TransCubeGroundAttackState::Update(TransCube* state)
-{
+void TransCubeGroundAttackState::Update(TransCube* state){
 	CoolTime++;
-	if (CoolTime > 60)
-	{
+	if (CoolTime > 60){
 		Fire(state);
 		BulletCount++;
 		CoolTime = 0;
 	}
-	if (BulletCount>=15)
-	{
+	if (BulletCount>=15){
 		state->StateFlag(true);
 	}
-
 }
 
-void TransCubeGroundAttackState::Draw(TransCube* state, ViewProjection view)
-{
-
-
+void TransCubeGroundAttackState::Draw(TransCube* state, ViewProjection view){
 }
 
-void TransCubeGroundAttackState::SetParent(const WorldTransform* parent)
-{
+void TransCubeGroundAttackState::SetParent(const WorldTransform* parent){
 }
 
-void TransCubeGroundAttackState::Deleate(TransCube* state)
-{
-	for (TransCubeGroundAttack* bullet : state->GetGroundBullets())
-	{
+void TransCubeGroundAttackState::Deleate(TransCube* state){
+	for (TransCubeGroundAttack* bullet : state->GetGroundBullets()){
 		//bullet->SetDead(true);
 		//delete bullet;
     }
